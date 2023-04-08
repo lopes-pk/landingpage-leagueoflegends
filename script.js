c2ae20e70e303785ad004f9b10672a3059b6eb5d
lopes-pk <arthurlopes558@gmail.com>
2023-04-07 22:49:07 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-    $("#screen-home").html("Novo conteúdo");
-  });
+const textoAnimado = document.querySelectorAll('#texto-sobre');
+
+window.addEventListener('scroll', function(){
+  console.log(window.scrollY)
+  if(window.scrollY > 100){
+    textoAnimado[0].classList.add("animate__animated", "animate__zoomIn");
+    
+  }
+  if(window.scrollY > 500){
+    textoAnimado[1].classList.add("animate__animated", "animate__zoomIn");
+    
+  }
+  // console.log(textoAnimado.classList)
+})
